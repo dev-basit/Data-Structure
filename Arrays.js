61b71bf9593f80082e2d40ea9a86f761d8b37907
Abdul Basit <dev.basit2020@gmail.com>
2022-11-11 20:29:21 +0500

    first commit

---
+++ Arrays.js
@@ -0,0 +1,25 @@
+class MyArray {
+  constructor(length = 0, data = []) {
+    this.length = length;
+    this.data = data;
+  }
+  print() {
+    //    console.log(this.data); // is k through undefined b show hta ta, jb element ko remove kia jata to, islye isko comment krdia
+    for (let i = 0; i < this.length; i++) {
+      console.log(this.data[i]);
+    }
+  }
+
+  getlength() {
+    return this.length;
+  }
+
+  indexof(value) {
+    for (let i = 0; i < this.length; i++) {
+      if (this.data[i] === value) {
+        return i;
+      }
+    }
+    return -1;
+  }
+}
