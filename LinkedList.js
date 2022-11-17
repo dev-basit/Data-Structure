19c675819c4e682e5444ba4338210575e780f069
Abdul Basit <dev.basit2020@gmail.com>
2022-11-17 18:13:50 +0500

    LinkedList Class

---
+++ LinkedList.js
@@ -0,0 +1,40 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(data) {
+    this.head = new Node(data);
+    this.tail = this.head;
+    this.length = 1;
+    this.printList();
+  }
+
+  // toString or toArray
+  printList() {
+    let array = [];
+    let currentNode = this.head;
+
+    while (currentNode != null) {
+      array.push(currentNode.data);
+      currentNode = currentNode.next;
+    }
+    console.log(array);
+
+    return array;
+  }
+
+  // append
+  addLast(data) {
+    let newNode = new Node(data); // mosh nai if else ko oper n6y lka h
+    if (this.tail) {
+      this.tail.next = newNode;
+      this.tail = newNode;
+      this.length++;
+      this.printList();
+    } else this.addBeginning(data);
+  }
+}
