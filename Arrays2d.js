1f6c5e243ae1ffcd571bc2ba9219ce891c74d150
Abdul Basit <dev.basit2020@gmail.com>
2024-02-23 11:05:20 +0500

    2d Array / Matrix

---
+++ Arrays2d.js
@@ -0,0 +1,35 @@
+const directions = [
+  [-1, 0], // up
+  [0, 1], // right
+  [1, 0], // down
+  [0, -1], // left
+];
+
+function traversalDFS(matrix) {
+  const seen = new Array(matrix.length).fill(0).map(() => new Array(matrix[0].length).fill(false));
+  const values = [];
+
+  dfs(matrix, 0, 0, seen, values);
+
+  return values;
+}
+
+function dfs(matrix, row, col, seen, values) {
+  if (row < 0 || col < 0 || row >= matrix.length || col >= matrix.length || seen[row][col]) return;
+
+  values.push(matrix[row][col]);
+  seen[row][col] = true;
+
+  for (let i = 0; i < directions.length; i++) {
+    const currentDir = directions[i];
+    dfs(matrix, row + currentDir[0], col + currentDir[1], seen, values);
+  }
+}
+
+console.log(
+  traversalDFS([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
