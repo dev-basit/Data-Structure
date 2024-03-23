020b9e213e79eed8c88a56ef46cd912bac610594
Abdul Basit <dev.basit2020@gmail.com>
2024-03-23 20:57:46 +0500

    Queue: Array Implemnetation

--- Queue_array_based.js
+++ Queue_array_based.js
@@ -1,70 +1,61 @@
 // Array implementation, yaha p ap built in length property b use kr skty ho
 class Queue {
-  constructor(data) {
-    this.data = data;
-    this.length = 1;
+  constructor(size = 0) {
+    this.items = new Array(size).fill(null); // simulating statically typed programming languages, they have fixed size
+    this.front = 0;
+    this.rear = 0;
+    this.count = 0;
   }
 
-  printQueue() {
-    let output = [];
-    for (let i = 0; i < this.length; i++) output.push(this.data[i]);
-
-    console.log("Queue elements are: ", output);
-  }
-
-  // add item at the end of queue
+  // Add item at the rear of queue
   enqueue(data) {
-    this.data[this.length++] = data;
-    this.printQueue();
+    if (this.count === this.items.length || this.rear === this.items.length)
+      throw new Error("Queue is full. Queue Overflow Exception");
+
+    this.items[this.rear++] = data;
+    this.count++;
   }
 
-  // remove item from the top or beginning
+  // Remove item from the top or front of queue, return the item removed
   dequeue() {
-    this.length--;
-    for (let i = 0; i < this.length; i++) {
-      this.data[i] = this.data[i + 1];
-    }
+    if (this.front >= this.rear) throw new Error("No Elements present.");
 
-    this.data.length--;
-    this.printQueue();
-    // return temp;
+    const item = this.items[this.front];
+    this.items[this.front] = null;
+    this.front++;
+    this.count--;
+
+    return item;
   }
 
-  // return beginning element, don't remove it
+  // return rear most element, don't remove it
   peek() {
-    let temp = this.data[0];
-    this.printQueue();
-    return temp;
+    if (this.count === 0) throw new Error("No Elements present.");
+
+    return this.items[this.rear - 1];
   }
 
   isEmpty() {
-    if (this.length) return false;
+    // Following 2 lines can be written as: return this.count
 
-    return true;
+    if (this.count > 0) return false;
+    else return true;
+  }
+
+  printQueue() {
+    console.log("");
+    console.log(this);
+    console.log("");
   }
 }
 
-let myQueue = new Queue([1]);
-myQueue.enqueue(2);
-console.log(myQueue);
-myQueue.enqueue(5);
-console.log(myQueue);
-myQueue.enqueue(15);
-console.log(myQueue);
-console.log("Queue is empty? ", myQueue.isEmpty());
-console.log(myQueue.dequeue());
-console.log(myQueue);
-console.log(myQueue.printQueue());
-console.log(myQueue.peek());
-console.log(myQueue);
-console.log("Queue is empty? ", myQueue.isEmpty());
-// console.log(myQueue);
-console.log(myQueue.dequeue());
-console.log(myQueue);
-console.log(myQueue.dequeue());
-myQueue.enqueue(5);
-console.log(myQueue);
-myQueue.enqueue(15);
-console.log(myQueue);
-console.log("Queue is empty? ", myQueue.isEmpty());
-console.log(myQueue);
+let queue = new Queue(5);
+queue.printQueue();
+// queue.enqueue(2);
+// queue.printQueue();
+// queue.enqueue(5);
+// queue.printQueue();
+// queue.dequeue();
+// queue.printQueue();
+// console.log("peek element: ", queue.peek());
+// console.log("Is queue empty? ", queue.isEmpty());
